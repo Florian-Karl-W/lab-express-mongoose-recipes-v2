2029cc4663b340a58e2c3c6c83edc8df2415f218
Florian Wunsch <Florian95.wunsch@gmail.com>
2025-05-14 15:24:32 +0200

    done

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 const express = require("express");
 const logger = require("morgan");
-
+const mongoose = require("mongoose");
 const app = express();
+const Recipe = require("./models/Recipe.model")
 
 // MIDDLEWARE
 app.use(logger("dev"));
@@ -11,7 +12,12 @@ app.use(express.json());
 
 // Iteration 1 - Connect to MongoDB
 // DATABASE CONNECTION
+const MONGODB_URI = "mongodb://127.0.0.1:27017/express-mongoose-recipes-dev";
 
+mongoose
+  .connect(MONGODB_URI)
+  .then((x) => console.log(`Connected to Mongo! Database name: "${x.connections[0].name}"`))
+  .catch((err) => console.error("Error connecting to mongo", err));
 
 
 // ROUTES
@@ -23,24 +29,79 @@ app.get('/', (req, res) => {
 
 //  Iteration 3 - Create a Recipe route
 //  POST  /recipes route
+app.post('/recipes',(req, res) => {
+    const recipeData = req.body;
 
+    Recipe.create(recipeData)
+    .then((newRecipe)=> {
+        res.json(newRecipe)
+    })
+    .catch((error) => {
+        console.log("Nothing was created", error)
+        res.status(500).json({ error: "Failed to create a new pizza" });
+    })
+})
 
 //  Iteration 4 - Get All Recipes
 //  GET  /recipes route
-
+app.get('/recipes', (req, res) => {
+    Recipe.find()
+    .then((allRecipes)=>{
+        res.json(allRecipes)
+    })
+    .catch((error) => {
+        console.log("Nothing was get", error)
+        res.status(500).json({ error: "Failed" });
+    })
+})
 
 //  Iteration 5 - Get a Single Recipe
 //  GET  /recipes/:id route
+app.get('/recipes/:id', (req, res) => {
+    const {recipeId} = req.params;
 
+    Recipe.findById(recipeId)
+    .then((foundRecipe) => {
+        req.json(foundRecipe)
+    })
+    .catch((error) => {
+        console.log("Error getting recipes", error)
+        res.status(500).json({ error: "Failed" });
+    })
+})
 
 //  Iteration 6 - Update a Single Recipe
 //  PUT  /recipes/:id route
+app.get('recipes/:id', (req, res) => {
+    const {recipeId} = req.params;
+    const updatedData = req.body;
+
+    Recipe.findByIdAndUpdate(recipeId, updatedData, {new: true})
+    .then((updatedRecipe) => {
+        res.json(updatedRecipe)
+    })
+     .catch((error) => {
+        console.log("Error updating recipes", error)
+        res.status(500).json({ error: "Failed" });
+    })
+})
 
 
 //  Iteration 7 - Delete a Single Recipe
 //  DELETE  /recipes/:id route
+app.delete('/recipes/:id', (req, res) => {
+    const {recipeId} = req.params
 
+    Recipe.findByIdAndDelete(recipeId)
 
+    .then((deleteRecipe) => {
+        res.json(deleteRecipe)
+    })
+    .catch((error) => {
+        console.log("Error deleting recipes", error)
+        res.status(500).json({ error: "Failed" });
+    })
+})
 
 // Start the server
 app.listen(3000, () => console.log('My first app listening on port 3000!'));
--- models/Recipe.model.js
+++ models/Recipe.model.js
@@ -1,1 +1,16 @@
-// Your code here ...
+const mongoose = require("mongoose")
+const Schema = mongoose.Schema;
+
+const recipeSchema = new Schema ({
+    title: String,
+    instruction: String,
+    level: String,
+    ingredients: [String],
+    image: String,
+    duration: Number,
+    isArchived: Boolean,
+    created: Date
+})
+
+const Recipe = mongoose.model("Recipe", recipeSchema);
+module.exports = Recipe;
